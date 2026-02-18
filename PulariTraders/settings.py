bc747050c935dece227c67954b6c09674a6cb43c
prileep <prileep@gmail.com>
2026-02-18 14:51:21 +0530

    Added dashboard template

--- PulariTraders/settings.py
+++ PulariTraders/settings.py
@@ -53,11 +53,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'PulariTraders.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ [os.path.join(BASE_DIR, 'templates')]],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'PulariTraders.wsgi.application'
 
